198775c095e97040450c115530c7774d0aea6b62
Chinonso <chinonsochibuzor2277@gmail.comgit config --global user.email chinonsochibuzor2277@gmail.comgit config --global user.email chinonsochibuzor2277@gmail.com>
2022-10-25 10:41:49 +0100

    React-blog

---
+++ src/header/Header.jsx
@@ -0,0 +1,13 @@
+import  './header.css'
+
+export default function Header() {
+  return (
+    <div className='header'>
+      <div className="headerTitles">
+        <span className="headerTitleSm">React & Node</span>
+        <span className="headerTitleLg">Blog</span>
+      </div>
+        <img src="https://images.unsplash.com/photo-1616531980827-1f21b9d387a2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80" alt="" className="headerImg" />
+    </div>
+  )
+}
