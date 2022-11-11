a98ef9e5d1a02b5a7ac771fae91bb78542e6c505
Chinonso <chinonsochibuzor2277@gmail.comgit config --global user.email chinonsochibuzor2277@gmail.comgit config --global user.email chinonsochibuzor2277@gmail.com>
2022-11-11 16:06:17 +0100

    update

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:9800/api"
+axios.defaults.baseURL = "https://react-blog-gt09.onrender.com"
 axios.defaults.headers.authorization = localStorage.getItem("token")
 
 export default axios;
